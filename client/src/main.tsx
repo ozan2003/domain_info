2b3a75e0c04a2bcc14eb9a649565cb37c163d8a8
Ozan Malci <ozan41094@gmail.com>
2026-07-01 17:44:22 +0300

    feat: setup client with dark theme with vite proxy

---
+++ client/src/main.tsx
@@ -0,0 +1,19 @@
+/**
+ * @file main.tsx
+ * @fileoverview Application entry point. Mounts the React root.
+ *
+ * @author Ozan Malcı
+ */
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+const rootElement = document.getElementById("root")!;
+const root = createRoot(rootElement);
+
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+);
